347a68f83b1f7330d0eff5f9b3a04814b006768f
Rhys Pegrume <pegrume88@gmail.com>
2023-06-20 15:42:03 +0000

    added event listner to save input btn

---
+++ index.js
@@ -0,0 +1,8 @@
+// function saveLead(){
+//     console.log("button clicked")
+// }
+
+let inputBtn = document.getElementById("input-btn")
+inputBtn.addEventListener("click", function(){
+    console.log("button clicked")
+})
