35d65b7264a349cdb35243308e098861f026bf09
Rhys Pegrume <pegrume88@gmail.com>
2023-06-21 13:22:52 +0000

    added render leads function to render list items entered in the input

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 
-let myLeads = ["www.awesomeleads.com", "www.epicleads.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function(){
-    myLeads.push(inputEL.value)
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
 })
 
 function renderLeads() {
     let listItems = ""
 
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li> "
+        listItems += "<li><a target='_blank' href=' " + myLeads[i] +"'>" + myLeads[i] + "</a></li> "
         
     }
 
     ulEl.innerHTML = listItems
 }
-renderLeads()
